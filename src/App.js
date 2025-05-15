275fb74c52ba1c4dd4e345ded6804f741a74d9da
Sunil Rana <sunilmaxout21@gmail.com>
2025-05-16 01:43:20 +0530

    removed form files

--- src/App.js
+++ src/App.js
@@ -1,13 +1,12 @@
 
 import './App.css';
 import Product from './Component/Product';
-import Form from './Component/Form'
 function App() {
   return (
     <div className="App">
          <div> creating form here </div>
           <div>
-             <Form />
+             
              <Product />
           </div>
     </div>
